07e62c3e37e122077c4e9b1f0de31019de587ed0
Cannie511 <Canh51102@gmail.com>
2024-04-29 23:12:55 +0700

    meeting online

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const socketIo = require('socket.io');
+const { createServer } = require('http');
+const app = express();
+const port  = 3000;
+const server = createServer(app)
+const { Server } = require("socket.io");
+const io = socketIo(server)
+const { v4: uuidV4 } = require('uuid')
+//import libs
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+app.get('/', (req,res)=>{
+    res.redirect(`/${uuidV4()}`)
+})
+app.get('/:room',(req, res)=>{
+    res.render('index', {room: req.params.room})
+})
+io.on('connection', (socket) => {
+    console.log('New client connected');
+    socket.on('chat message', (msg) => {
+        // console.log('message: ' + msg);
+        io.emit('chat message', msg)
+    });
+    socket.on('join-room',(roomId, userId)=>{
+        console.log(roomId, userId)
+        socket.join(roomId)
+        socket.to(roomId).emit('user-connected', userId)
+        socket.on('disconnect',()=>{
+            io.emit('disconnected','client disconnect')
+            socket.to(roomId).emit('user-disconnected', userId);
+        })
+    })
+    
+});
+server.listen(port,()=>{
+    console.log(`socket is running on port ${port}`)
+})
